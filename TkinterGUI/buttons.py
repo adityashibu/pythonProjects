0216b0b13258e95ea366cddb3cc62ff44ac3c06e
Aditya Shibu <as2397@hw.ac.uk>
2023-12-25 22:01:23 +0400

    Completed basic button functions

--- TkinterGUI/buttons.py
+++ TkinterGUI/buttons.py
@@ -19,4 +19,46 @@ button1 = Button(window,
                  command=click)
 button1.pack()
 
+'''
+Change font size and family using the font attribute
+'''
+button2 = Button(window,
+                 text="Click me",
+                 font=("Comic Sans", 30))
+button2.pack()
+
+'''
+Background and foreground color can be changed using the same commands as before, the bg and fg attributes
+'''
+button3 = Button(window,
+                 text="Click me!",
+                 bg="Red",
+                 fg="White")
+button3.pack()
+
+'''
+Change the format of the button when clicked using the activebackground and activebackground attributes
+'''
+button4 = Button(window,
+                 text="Click me!",
+                 bg="Red",
+                 fg="white",
+                 activebackground="White",
+                 activeforeground="Red")
+button4.pack()
+
+'''
+To disable interaction with a button use the state attribute, ACTIVE by default
+'''
+button5 = Button(window,
+                 text="Click me!",
+                 state=DISABLED)
+button5.pack()
+
+'''
+As before an image can be added to a button by first converting it to a photoimage and then using the image
+attribute of the button to add it to the button, if you want to add text and image to the button then use
+the compound attribute to add the image and the text
+'''
+
 window.mainloop()
