d64057919b4e7f192082c2280bc835f9f45f67dd
Aditya Shibu <as2397@hw.ac.uk>
2023-12-25 19:07:14 +0400

    Added icon to the window

--- TkinterGUI/main.py
+++ TkinterGUI/main.py
@@ -7,7 +7,11 @@ Widgets: GUI elements such as buttons, textboxes, labels etc
 Windows: Container to hold these widgets
 '''
 
-# Simple window 
-window = Tk()
-# To show the window
-window.mainloop()
+window = Tk() # Simple window 
+window.geometry("500x500") # For setting size of the window, use the geometry attribute 
+window.title("Basic window") # To set the title of the window
+
+icon = PhotoImage(file="TKinterGUI/sun.png") # Create a photoimage from the given photo so it can be used by TKinter for the window icon
+window.iconphoto(True, icon) # Set the given icon as the icon of the window
+
+window.mainloop() # To show the window
