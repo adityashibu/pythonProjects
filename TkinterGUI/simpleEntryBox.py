b6a97ae97f5d0cadebef637de80bf5b2290a7b49
Aditya Shibu <as2397@hw.ac.uk>
2023-12-25 23:36:08 +0400

    Added a submit and delete button

--- TkinterGUI/simpleEntryBox.py
+++ TkinterGUI/simpleEntryBox.py
@@ -3,6 +3,31 @@ from tkinter import *
 window = Tk()
 window.title("Entry box")
 
+entry = Entry(window, 
+              font=("Courier", 50),
+              bg="black",
+              fg="#00ff00")
+entry.pack(side=LEFT)
 
+# Submit function
+def submit():
+    name = entry.get()
+    print(f"Hello, {name}")
+    
+# Submit button
+submitBtn = Button(window,
+                   text="Submit",
+                   command=submit)
+submitBtn.pack(side=RIGHT)
+
+# Delete function
+def delete():
+    entry.delete(0,END)
+
+# Delete button
+deleteBtn = Button(window, 
+                   text="Delete",
+                   command=delete)
+deleteBtn.pack(side=LEFT)
 
 window.mainloop()
