b7994b11d053f2f450edb508c0588fb042b4716d
Aditya Shibu <as2397@hw.ac.uk>
2023-12-25 19:09:34 +0400

    Set background color

--- TkinterGUI/main.py
+++ TkinterGUI/main.py
@@ -13,5 +13,6 @@ window.title("Basic window") # To set the title of the window
 
 icon = PhotoImage(file="TKinterGUI/sun.png") # Create a photoimage from the given photo so it can be used by TKinter for the window icon
 window.iconphoto(True, icon) # Set the given icon as the icon of the window
+window.config(background="light blue") # Set color for the window
 
 window.mainloop() # To show the window
