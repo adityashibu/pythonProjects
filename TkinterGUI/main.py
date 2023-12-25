00e9161f992c13d800192511fddea9960d9633c8
Aditya Shibu <as2397@hw.ac.uk>
2023-12-25 18:59:54 +0400

    Added code for displaying basic window

---
+++ TkinterGUI/main.py
@@ -0,0 +1,13 @@
+from tkinter import * 
+
+'''
+Difference between windows and widgets
+
+Widgets: GUI elements such as buttons, textboxes, labels etc
+Windows: Container to hold these widgets
+'''
+
+# Simple window 
+window = Tk()
+# To show the window
+window.mainloop()
