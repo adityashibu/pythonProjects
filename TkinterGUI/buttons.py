082bbbe0f0f20923e5bab42b6c20e06f6bf6d216
Aditya Shibu <as2397@hw.ac.uk>
2023-12-25 21:18:08 +0400

    Added function for callback when a button is clicked

--- TkinterGUI/buttons.py
+++ TkinterGUI/buttons.py
@@ -3,5 +3,20 @@ from tkinter import *
 window = Tk()
 window.title("Buttons")
 
+button = Button(window, 
+                text="Click me!",
+                )
+button.pack()
+
+def click():
+    print("You clicked the button :P")
+
+'''
+Tell the button what to do using the command attribute
+'''
+button1 = Button(window,
+                 text="Click me!",
+                 command=click)
+button1.pack()
 
 window.mainloop()
